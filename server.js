d21b2828347b8b771d055a694bec3b03ef202582
Masmics <cherlocker3113@gmail.com>
2019-01-13 19:05:44 -0800

    exported server function to index.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const http = require('http');
+var http = require('http');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+function start() {
+  function onRequest(request, response) {
+    console.log('Request recieved.');
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello World');
+    response.end();
+  }
 
 
-function say(word) {
-  console.log(word);
+  http.createServer(onRequest).listen(8888);
+
+  console.log('Server has started.');
 }
+exports.start = start;
 
-function execute(someFunction, value) {
-  someFunction(value);
-}
-
-execute(say, 'Hello');
