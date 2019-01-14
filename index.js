f8a77b3eadc1236814d820f9949ec933e1166d0b
Masmics <cherlocker3113@gmail.com>
2019-01-13 20:02:49 -0800

    handlers returning content

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var server = require('./server');
 var router = require('./router');
+var requestHandlers = require('./requestHandlers');
+
+var handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
 
 //**both of the following are function names**
-server.start(router.route);
+server.start(router.route, handle);
 
 
 
