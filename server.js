d222fb1b1d627c2d5c781292f3616af9be46c515
Masmics <cherlocker3113@gmail.com>
2019-01-14 07:41:25 -0800

    starting upload function

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var http = require('http');
 var url = require('url');
+var formidable = require('formidable');
+var sys = require('sys');
 
 //**adding handle paramater to start function**
 function start(route, handle) {
@@ -23,8 +25,28 @@ function start(route, handle) {
     });
     
   }
-  http.createServer(onRequest).listen(8888);
-  console.log('Server has started.');
+  // http.createServer(onRequest).listen(8888);
+  // console.log('Server has started.');
+  http.createServer(function(req, res) {
+    if(req.url == '/u[load' && req.method.toLowerCase() == 'post') {
+      var form = new formidable.incomingForm();
+      form.parse(req, function(error, fields, files) {
+        res.writeHead(200, { 'Content-Type': 'Text/Plain' });
+        res.write('received upload: \n\n');
+        res.end(sys.inspect({ fields: fields, files: files }));
+      });
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'Text/html' });
+    res.end(
+      '<form action="/upload" enctype="multipart/form-data" ' +
+      'method="post">' +
+      '<input type="text" name="title"><br>' +
+      '<input type="file" name="upload" multiple="multiple"><br>' +
+      '<input type="submit" value="Upload">' +
+      '</form>'
+    );
+  }).listen(8888);
 }
 exports.start = start;
 
