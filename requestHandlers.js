f4cacadca1248bb514938099a4420f42ae595178
Masmics <cherlocker3113@gmail.com>
2019-01-13 20:17:53 -0800

    blocking/non-blocking

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,22 @@
+var exec = require('child_process').exec;
+
 function start() {
   console.log('Request handler "start" was called.');
-  return 'Hello start';
+  var content = 'empty';
+
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  });
+  return content;
+
+  // function sleep(milliSeconds) {
+  //   var startTime = new Date().getTime();
+  //   while(new Date().getTime() < startTime + milliSeconds);
+  // }
+
+  // //**wait 10 sec + return 'Hello start'
+  // sleep(10000);
+  // return 'Hello start';
 }
 
 function upload() {
