d45c3e28a691639a04edfea119adde262c0a277f
Masmics <cherlocker3113@gmail.com>
2019-01-13 20:54:11 -0800

    handling post data complete

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ var url = require('url');
 //**adding handle paramater to start function**
 function start(route, handle) {
   function onRequest(request, response) {
+    var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    //**passing handle object into route()
-    route(handle, pathname, response);
+    request.setEncoding('utf8');
+
+    request.addListener('data', function(postDataChunk) {
+      postData =+ postDataChunk;
+      console.log('Recieved POST data chunk ' + postDataChunk + '.');
+    });
+
+    request.addListener('end', function() {
+      //**passing handle object into route()
+      route(handle, pathname, response, postData);
+    });
+    
   }
   http.createServer(onRequest).listen(8888);
   console.log('Server has started.');
